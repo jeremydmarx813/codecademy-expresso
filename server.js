bf3ef968616c409206e9c74939708522d56c6795
jeremydmarx <jeremydmarx@gmail.com>
2021-08-18 15:21:10 -0500

    initial cleanup, serving serving file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const errorhandler = require('errorhandler');
+// const errorhandler = require('errorhandler');
 const morgan = require('morgan');
 const express = require('express');
-
+const path = require('path');
 const apiRouter = require('./api/api');
 
 const app = express();
@@ -14,7 +14,11 @@ app.use(cors());
 
 app.use('/api', apiRouter);
 
-app.use(errorhandler());
+// app.use(errorhandler());
+app.use('/public', express.static('public'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
