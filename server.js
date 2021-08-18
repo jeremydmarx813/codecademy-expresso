72573d3f71f8f68c07ca1066c770b4344959051a
jeremydmarx <jeremydmarx@gmail.com>
2021-08-18 14:50:39 -0500

    added start script

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const apiRouter = require('./api/api');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.use('/api', apiRouter);
